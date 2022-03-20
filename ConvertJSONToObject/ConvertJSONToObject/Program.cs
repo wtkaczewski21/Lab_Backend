fbbca538d1243e9ac4f2b2f4fa51ea7fe2bd720a
nejski <wojtalokt@gmail.com>
2022-03-20 14:43:19 +0100

    Convert JSON To Object

---
+++ ConvertJSONToObject/ConvertJSONToObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ConvertJSONToObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            string userJson = "{\"name\":\"John\",\"age\":21}";
+
+            Person person = JsonConvert.DeserializeObject<Person>(userJson);
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+
+        public class Person
+        {
+            public string Name;
+            public int Age;
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+    }
+}

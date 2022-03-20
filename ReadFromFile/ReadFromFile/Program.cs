f7a76909ad2e71bae4d2c5128d19fe5ca5defaeb
nejski <wojtalokt@gmail.com>
2022-03-20 13:39:55 +0100

    Read From File

---
+++ ReadFromFile/ReadFromFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace ReadFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"TextFile.txt");
+            string text = File.ReadAllText(path);
+
+            Console.WriteLine(text);
+        }
+    }
+}

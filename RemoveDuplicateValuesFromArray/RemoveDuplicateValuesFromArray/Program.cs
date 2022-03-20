e484e34ca8d5c31580e7b60bb557526fd12c1f2d
nejski <wojtalokt@gmail.com>
2022-03-20 15:06:00 +0100

    Remove Duplicate Values From Array

---
+++ RemoveDuplicateValuesFromArray/RemoveDuplicateValuesFromArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicateValuesFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 5, 5, 5 };
+            HashSet<int> newArr = new HashSet<int>(arr);
+            int[] result = new int[newArr.Count];
+            newArr.CopyTo(result);
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}

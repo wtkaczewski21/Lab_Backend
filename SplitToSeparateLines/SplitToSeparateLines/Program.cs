fb308a7bbd56f7b8d8c50ba6057097ebec256d27
nejski <wojtalokt@gmail.com>
2022-03-20 14:16:54 +0100

    Split To Separate Lines

---
+++ SplitToSeparateLines/SplitToSeparateLines/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SplitToSeparateLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Tekst do rozbicia na osobne linie";
+
+            string[] subs = text.Split(' ');
+
+            int counter = 1;
+
+            foreach (var sub in subs)
+            {             
+                Console.WriteLine(counter + " " + sub);
+                counter++;
+            }
+        }
+    }
+}

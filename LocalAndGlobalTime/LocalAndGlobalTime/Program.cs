f82c1bc28c7e87e7758cf49832fad1c16060afd6
nejski <wojtalokt@gmail.com>
2022-03-20 14:04:36 +0100

    Local And Global Time

---
+++ LocalAndGlobalTime/LocalAndGlobalTime/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LocalAndGlobalTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string localDate = DateTime.Now.ToString("HH:mm:ss");
+            string utcDate = DateTime.UtcNow.ToString("HH:mm:ss");
+            Console.WriteLine(localDate);
+            Console.WriteLine(utcDate);
+        }
+    }
+}

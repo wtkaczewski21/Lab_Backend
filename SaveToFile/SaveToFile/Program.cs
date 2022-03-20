44f4781777a15b9ee823061a4d3566e1a77802ac
nejski <wojtalokt@gmail.com>
2022-03-20 13:41:08 +0100

    Save To File

---
+++ SaveToFile/SaveToFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace SaveToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            File.WriteAllText("TextFile.txt", text);
+        }
+    }
+}
